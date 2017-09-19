cf19e118fdc61caab24dda142b374c9badc4d099
Chris Nunez <cenunez47@gmail.com>
2017-09-19 17:39:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,14 @@
 def my_each(array)
+  if block_given?
   x = 0
-
   while x < array.length
     yield array[x]
     x += 1
   end
-
   array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # def hello_t(array)
